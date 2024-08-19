cc9415beb613b701b78bc5ea9c51d1eec0a0afaf
Артем Петров <fittedorangeofficial@mail.ru>
2024-08-19 15:18:16 +0300

    finished

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use std::cmp::Ordering;
+use std::io;
+use rand::Rng;
+
+fn main() {
+    println!("Hello! Please guess a number");
+
+    let secret_number = rand::thread_rng()
+        .gen_range(1..=100);
+    // println!("The secret number is: {secret_number}");
+
+    loop {
+        println!("Write the number:");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        //.expect("Please type a number!");
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!"); 
+                break;
+            }
+        }
+    }
+}
